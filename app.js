65c6d780f803bc4d01d9eec5a818648fc0b55dc1
KCvandenBergh <89838896+KCvandenBergh@users.noreply.github.com>
2023-02-26 22:56:29 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const classifier = knnClassfier.create()
+const video = document.getElementById("webcam");
+
+let net
+
+async function app(){
+    console.log("loading")
+}
+
+
+// window.addEventListener("load", function(){
+
+// var classifier, label, prob;
+// var img = new Image(300,350);
+
+// function setup(){
+//     img.src = ".jpg";
+//     document.body.appendChild(img);
+
+// }
+
+// setup();
+
+// })
+
+
+const label = document.getElementById("label");
+
+const labelOneBtn = document.querySelector("#labelOne");
+const labelTwoBtn = document.querySelector("#labelTwo");
+const labelThreeBtn = document.querySelector("#labelThree");
+const trainbtn = document.querySelector("#train");
+
+labelOneBtn.addEventListener("click", () => console.log("button 1"));
+labelTwoBtn.addEventListener("click", () => console.log("button 2"));
+labelThreeBtn.addEventListener("click", () => console.log("button 3"));
+
+trainbtn.addEventListener("click", () => console.log("train"));
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+            video.srcObject = stream;
+        })
+        .catch((err) => {
+            console.log("Something went wrong!");
+        });
+}
+
+label.innerText = "Ready when you are!";
